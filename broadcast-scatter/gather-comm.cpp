e2bfe3b5a139505b9ef466ae7f968ef27cf7a0bf
Erick-xps-ub <juarez.erick16@csu.fullerton.edu>
2020-02-17 11:57:01 -0800

    Add all-gather example

--- broadcast-scatter/gather-comm.cpp
+++ broadcast-scatter/gather-comm.cpp
@@ -14,17 +14,11 @@ int main(int argc, char *argv[]){
     int a[5]; 
     int b = rank;
     for(int i = 0; i < 5; i++) { a[i] = 0; }
-    printf("Process of rank %d has array a =  [%d, %d, %d, %d, %d] and b = %d\n", rank, a[0], a[1], a[2], a[3], a[4], b);
-
-
-    // Rank 2 modifies array values 
-    if(rank == 2){
-        for(int i = 0; i < n; i++){ a[i] = i; }
-    }
+    printf("1. Process of rank %d has array a =  [%d, %d, %d, %d, %d] and b = %d\n", rank, a[0], a[1], a[2], a[3], a[4], b);
 
     // Rank 2 gathers values from other processes
     MPI_Gather(&b, 1, MPI_INT, &a, 1, MPI_INT, 2, MPI_COMM_WORLD);  
-    printf("Process of rank %d has array a =  [%d, %d, %d, %d, %d] and b = %d\n", rank, a[0], a[1], a[2], a[3], a[4], b);
+    printf("2. Process of rank %d has array a =  [%d, %d, %d, %d, %d] and b = %d\n", rank, a[0], a[1], a[2], a[3], a[4], b);
 
     
     MPI_Finalize();
