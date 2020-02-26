b3f3b625ad22a5460925c4109b1156552f821cf7
Erick-xps-ub <juarez.erick16@csu.fullerton.edu>
2020-02-25 17:33:02 -0800

    Finish hw 4

--- hw-4/parallel-array.cpp
+++ hw-4/parallel-array.cpp
@@ -6,8 +6,11 @@ developed and tested using g++ version 5.3.0 (compiled wiht mpic++ to use MPI li
 
 Program has 3 parts
 1. Initialize an array of 64 elements with values of 0 using 8 threads 
-2. Use a parallel construct to add 2 * i to array[i] for i = 0 to i = 63
-3. 
+2. Add 2 * i to array[i] for i = 0 to i = 63
+3. Calculate number of odd values in array[] in the following way: 
+    1. Distribute iterations to threads in a cyclic manner (static / round-robin fashion)
+    2. Each thread computes the number of odd values from its allocated iterations
+    3. Master theread collects and adds the number of odd values and displays the result
 */
 #include<omp.h>
 #include<stdio.h>
@@ -18,19 +21,53 @@ int main(){
     // Initialize 
     const int SIZE = 64;
     int array[SIZE];
-    int tid; 
+    int tid, private_sum, odd_count;
     omp_set_num_threads(THREADS);
 
-    // Parallel region using the shared array 
-    #pragma omp parallel private(tid) shared(array)
+    // Parallel region for Problem 1 
+    #pragma omp parallel shared(array)
+    {
+        // Parallel for loop initializes all array values to 0
+        #pragma omp for schedule(static)
+        for (int i = 0; i < SIZE; i++){
+            array[i] = 0;
+        }
+    }
+
+    // Parallel region Problem 2
+    #pragma omp parallel shared(array) private(tid)
     {
         tid = omp_get_thread_num();
-        // Parallel for loop initializes all array values to 0
+        // Parallel for loop adds 2 * i to array[i] for i = 0 to i = 63
         #pragma omp for schedule(static, 1)
-        for (int i =0; i < SIZE; i++){
-            array[i] = 0;
+        for (int i = 0; i < SIZE; i++){
+            array[i] += 2 * i;
             printf("Thread: %d Array Index: %d Value: %d\n", tid, i, array[i]);
         }
+    }
+
+    // Parallel region for problem 3
+    #pragma omp parallel private(tid, private_sum) shared(array, odd_count)
+    {
+        tid = omp_get_thread_num();
+        private_sum = 0; 
+        // Parallel loop that finds number of odd numbers in its iterations privately 
+        #pragma omp for schedule(static, 1)
+        for (int i = 0; i < SIZE; i++){
+            if(array[i] % 2 != 0) { 
+                private_sum+= 1; 
+                }
+        }
+        // Collect all private counts
+        #pragma omp critical
+        {
+            odd_count+= private_sum;
+        }
+        // Master thread prints result
+        if(tid == 0){
+            printf("=========================\nOdd numbers in array[]: %d\n", odd_count);
+        }
+
     }
 
     return 0; 
